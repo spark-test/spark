c829092d50962c1d2aa8c5bd010fd0b6dfd6e991
hyukjinkwon <gurwls223@gmail.com>
2016-12-12 12:53:14 +0900

    print env

--- core/src/main/scala/org/apache/spark/rdd/PipedRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/PipedRDD.scala
@@ -73,6 +73,7 @@ private[spark] class PipedRDD[T: ClassTag](
     // so the user code can access the input filename
     if (split.isInstanceOf[HadoopPartition]) {
       val hadoopSplit = split.asInstanceOf[HadoopPartition]
+      println("++++++++++++" + hadoopSplit.getPipeEnvVars())
       currentEnvVars.putAll(hadoopSplit.getPipeEnvVars().asJava)
     }
 
