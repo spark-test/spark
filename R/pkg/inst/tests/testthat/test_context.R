869ad6080ad8ba1ea4fad9c9d11fddda7f2d6a78
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-03-03 09:52:59 -0500

    Fixup R syntax issues

--- R/pkg/inst/tests/testthat/test_context.R
+++ R/pkg/inst/tests/testthat/test_context.R
@@ -167,7 +167,7 @@ test_that("spark.lapply should perform simple transforms", {
   sparkR.session.stop()
 })
 
-test_that("add jar should work and allow usage of the jar on the driver node" {
+test_that("add jar should work and allow usage of the jar on the driver node", {
   sparkR.sparkContext()
 
   destDir <- paste0(tempdir(), "/", "testjar")
@@ -177,7 +177,7 @@ test_that("add jar should work and allow usage of the jar on the driver node" {
   spark.addJar(jarName, addToCurrentClassLoader = TRUE)
   testClass <- newJObject("sparkrTests.DummyClassForAddJarTest")
   expect_true(class(testClass) == "jobj")
-}
+})
 
 test_that("add and get file to be downloaded with Spark job on every node", {
   sparkR.sparkContext()
